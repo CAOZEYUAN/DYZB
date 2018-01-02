dc91f26273511f361de9731ece15c5ca8d2b6754
apple <apple@appledeiMac.local>
2018-01-02 16:10:17 +0800

    框架建立

---
+++ 斗鱼直播/斗鱼直播/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  斗鱼直播
+//
+//  Created by apple on 2018/1/2.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        UITabBar.appearance().tintColor = UIColor.orange
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+      
+    }
+
+  
+
+}
+
