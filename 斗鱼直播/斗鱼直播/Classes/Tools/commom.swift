06f0f014ac977e67c21301a283a9e9220402fb96
apple <apple@appledeiMac.local>
2018-01-05 17:02:12 +0800

    Home页面视图的建立

---
+++ 斗鱼直播/斗鱼直播/Classes/Tools/commom.swift
@@ -0,0 +1,15 @@
+//
+//  commom.swift
+//  斗鱼直播
+//
+//  Created by apple on 2018/1/5.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+import UIKit
+
+let kStatusBarH:CGFloat  = 20
+let kNavigationBarH : CGFloat = 44
+let kTitleViewH : CGFloat = 40
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
